827f48205c5b9824b699722c5c09992c5f1727b1
Guille <guillejuanmanuel@gmail.com>
2020-07-09 23:17:09 -0300

    Maquetado de form y integracion de php

---
+++ js/logic.js
@@ -0,0 +1,38 @@
+
+//Boton Up
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 100){
+        document.querySelector('.expandir').classList.add('show');
+    }else{
+        document.querySelector('.expandir').classList.remove('show');
+    }
+}
+
+document.querySelector('.expandir').addEventListener('click',() => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
+//Validar Formulario
+
+function campoRequerido(elemento){
+    let expresion = "[A-Za-z]";
+    if(elemento.value != "" && expresion.test(elemento.value)){//si esta vacio
+        elemento.className = "form-control is-valid";
+    }else{
+        elemento.className = "form-control is-invalid";
+    }
+}
+
+function validarMail(elemento){
+    let expresionRegular = /\w+@\w+\.[a-z A-Z]/;
+    if(elemento.value != "" && expresionRegular.test(elemento.value)){//si esta vacio
+        elemento.className = "form-control is-valid";
+    }else{
+        elemento.className = "form-control is-invalid";
+    }
+}
---
+++ php/correo.php
@@ -0,0 +1,13 @@
+<?php
+    $destinatario = 'guillejuanmanuel@gmail.com';//cooreo a donde se enviara
+    $nombre = $_POST['nombre'];
+    $asunto = $_POST['consulta'];
+    $email = $_POST['email'];
+
+    $header = "Enviado desde el cv";
+    $mensajeCompleto = $asunto . "\n Atentamente:". $nombre;
+
+    mail($destinatario, $mensajeCompleto, $header);
+    echo"<script>alert('Correo enviado exitosamente')</script>";
+    echo"<script>setTimeout(\"location.href='index.html'\",1000</script>";
+?>
